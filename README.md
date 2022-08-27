4081dd0ade9c04df0c0d22ea85aefe40192a21f4
Luis Gabriel de M Fernandes <lgabrielmf@gmail.com>
2022-08-26 23:56:46 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# spring-kafka-consumer
-Example of Spring Consumer using Spring
---
+++ src/test/java/com/gabrielfernandes/estudo/spring/kafka/springkafkaconsumer/SpringKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielfernandes.estudo.spring.kafka.springkafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
